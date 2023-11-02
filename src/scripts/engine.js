551af4625a64964006b7e96074ec065d9ab0bb0c
Frankdias92 <frankmcdias@gmail.com>
2023-11-02 12:18:19 -0300

    :construction WIP

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,32 +1,66 @@
-const state = {
-  score:{
-    playerScore: 0,
-    enemyScore: 0,
-    scoreBox: document.querySelector('#'),
-  },
-  cardSprites: {
-    // avatar: 
-  },
-  fildCards: {
-    player: document.querySelector('#field-card-player'),
-    enemy: document.querySelector('#field-card-enemy'),
-  },
-  actions: {
+let avaibleCards = [1,2,3];
+let playerCard = null;
+let enemyCard = null;
+let battleResult = null;
 
-  },
+function updateCardsView() {
+  const playerCardElement = document.querySelector('#field-card-player');
+  const enemyCardElement = document.querySelector('#field-card-enemy');
+
+  playerCardElement.src = `/src/assets/icons/${playerCard}.png`;
+  enemyCardElement.src = `/src/assets/icons/${enemyCard}.png`;
+
+  const battleResultElement = document.querySelector('#battle-result');
+
+  if (battleResultElement === 'win') {
+    battleResultElement.textContent = "você venceu";
+  } else if (battleResultElement === 'lose') {
+    battleResultElement.textContent = "você perdeu";
+  } else if (battleResultElement === 'draw') {
+    battleResultElement.textContent = "empate";
+  } else {
+    battleResultElement.textContent = '';
+  }
 }
 
-const cardData = [
-  {
-    id: 0,
-    name: '',
-    type: '',
-    img: '',
-  },
-];
+function handleBattleButtonClick() {
+  const randomIdex = Math.floor(Math.random() * avaibleCards.length);
+  enemyCard = avaibleCards[randomIdex];
+
+  if (playerCard === enemyCard) {
+    battleResult = 'draw';
+  } else if (
+    (playerCard === 1 && enemyCard === 3) ||
+    (playerCard === 2 && enemyCard === 1) ||
+    (playerCard === 3 && enemyCard === 2)
+  ) {
+    battleResult = 'win';
+  } else {
+    battleResult = 'lose';
+  }
+
+  updateCardsView();
+}
+
+function addPlayerCardClickEvent() {
+  const playerCards = document.querySelector('.cardsPlayer');
+  playerCards.forEach(card => {
+    card.addEventListener('click', () => {
+      handlePlayerCardClick(card.textContent);
+    })
+  })
+}
 
 
+function addBattleButtonClickEvent() {
+  const battleButton = document.querySelector('#battleButton');
+  battleButton.addEventListener('click', handleBattleButtonClick);
+}
 
-function init () {}
+function init () {
+  updateCardsView();
+  addPlayerCardClickEvent();
+  addBattleButtonClickEvent();
+}
 
 init ();
