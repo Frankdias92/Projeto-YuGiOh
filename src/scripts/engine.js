3a59a3e87a85a5ff5fd1fec70e01ecd340962d24
Frankdias92 <frankmcdias@gmail.com>
2023-11-02 14:13:32 -0300

    :boom: subindo projeto

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,66 +1,163 @@
-let avaibleCards = [1,2,3];
-let playerCard = null;
-let enemyCard = null;
-let battleResult = null;
+const state ={
+  score:{
+      playerScore:0,
+      computerScore:0,
+      scoreBox: document.getElementById("score_points")
+  },
+  cardSprites:{
+      avatar: document.getElementById("card_image"),
+      name: document.getElementById("card_name"),
+      type: document.getElementById("card_type"),
+  },
+  fieldCards:{
+      player: document.getElementById("player_field_card"),
+      computer: document.getElementById("computer_field_card"),
+  },
+  playerSides: {
+  cardPlayer:"player_field_card",
+  cardComputer:"computer_field_card",
+  playerBox: document.querySelector("#player_cards"),
+  computerBox: document.querySelector("#computer_cards"),
+  player:"player_cards",
+  computer:"computer_cards"
+  },
+  actions:{
+      button:  document.getElementById("next_duel")
+  },
+}
 
-function updateCardsView() {
-  const playerCardElement = document.querySelector('#field-card-player');
-  const enemyCardElement = document.querySelector('#field-card-enemy');
+const cardData = [
+  {
+      id:0,
+      name: "Blue Eyes White Dragon",
+      type: "Paper",
+      img:"./src/assets/icons/dragon.png",
+      winOf:[1],
+      loseOf:[2]
+  },
+  {
+      id:1,
+      name: "Dark Magician",
+      type: "Rock",
+      img:"./src/assets/icons/magician.png",
+      winOf:[2],
+      loseOf:[0]
+  },
+  {
+      id:2,
+      name: "Exodia",
+      type: "Scissors",
+      img:"./src/assets/icons/exodia.png",
+      winOf:[0],
+      loseOf:[1]
+  }
+]
+async function removeAllCardsImages(){
+let { playerBox, computerBox} = state.playerSides
+let imgElements = computerBox.querySelectorAll("img")
+imgElements.forEach((img) => img.remove())
+imgElements = playerBox.querySelectorAll("img")
+imgElements.forEach((img) => img.remove())
+}
 
-  playerCardElement.src = `/src/assets/icons/${playerCard}.png`;
-  enemyCardElement.src = `/src/assets/icons/${enemyCard}.png`;
+async function checkDueResults(playerCardId,computerCardId){
+  
+  const playerCard = cardData[playerCardId]
+  if(playerCard.winOf.includes(computerCardId)){
+      state.score.playerScore++
+      await playAudio("win")
+      return "WIN"
+  }else if (playerCard.loseOf.includes(computerCardId)){
+      state.score.computerScore++
+      await playAudio("lose")
+      return "LOSE"
+  }
+  return "DRAW"
+}
 
-  const battleResultElement = document.querySelector('#battle-result');
+async function setCardsField(cardId){
+  await removeAllCardsImages()
+  let computerCardId = await getRandomCardId()
 
-  if (battleResultElement === 'win') {
-    battleResultElement.textContent = "você venceu";
-  } else if (battleResultElement === 'lose') {
-    battleResultElement.textContent = "você perdeu";
-  } else if (battleResultElement === 'draw') {
-    battleResultElement.textContent = "empate";
-  } else {
-    battleResultElement.textContent = '';
+  state.fieldCards.player.style.display = "block"
+  state.fieldCards.computer.style.display = "block"
+
+  state.fieldCards.player.src = cardData[cardId].img
+  state.fieldCards.computer.src = cardData[computerCardId].img
+
+  let duelResults = await checkDueResults(cardId,computerCardId)
+
+  await updateScore()
+  await drawButton(duelResults)
+}
+
+async function updateScore(){
+  state.score.scoreBox.innerText = `Win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`
+}
+
+async function resetDuel(){
+  state.cardSprites.avatar.src = ""
+  state.actions.button.style.display = "none"
+  state.fieldCards.computer.style.display = "none"
+  state.fieldCards.player.style.display = "none"
+  init()
+}
+
+async function drawButton(text){
+  state.actions.button.innerText = text
+  state.actions.button.style.display = "block"
+}
+
+async function drawSelectCard(index){
+  state.cardSprites.avatar.src = cardData[index].img
+  state.cardSprites.name.innerText = cardData[index].name
+  state.cardSprites.type.innerText = "Attibute: "+cardData[index].type
+}
+
+async function creatCardImage(IdCard, fieldSide){
+  const cardImage = document.createElement("img")
+  cardImage.setAttribute("height","100px")
+  cardImage.setAttribute("src","./src/assets/icons/card-back.png")
+  cardImage.setAttribute("data-id",IdCard)
+  cardImage.classList.add("card")
+
+  if(fieldSide===state.playerSides.player){
+      cardImage.addEventListener("click", ()=>{
+          setCardsField(cardImage.getAttribute("data-id"))
+      })
+      cardImage.addEventListener("mouseover", ()=>{
+          drawSelectCard(IdCard)
+      })
+
+  }
+  return cardImage
+}
+
+async function getRandomCardId(){
+  const randomIndex = Math.floor(Math.random()*cardData.length)
+  return cardData[randomIndex].id
+}
+
+async function drawCards(quantity, fieldSide){
+  for(let i=0; i<quantity; i++){
+      const randomIdCard = await getRandomCardId();
+      const cardImage = await creatCardImage(randomIdCard,fieldSide)
+
+      document.getElementById(fieldSide).appendChild(cardImage)
   }
 }
-
-function handleBattleButtonClick() {
-  const randomIdex = Math.floor(Math.random() * avaibleCards.length);
-  enemyCard = avaibleCards[randomIdex];
-
-  if (playerCard === enemyCard) {
-    battleResult = 'draw';
-  } else if (
-    (playerCard === 1 && enemyCard === 3) ||
-    (playerCard === 2 && enemyCard === 1) ||
-    (playerCard === 3 && enemyCard === 2)
-  ) {
-    battleResult = 'win';
-  } else {
-    battleResult = 'lose';
-  }
-
-  updateCardsView();
+async function playAudio(status){
+  const audio = new Audio(`./src/assets/audios/${status}.wav`)
+  audio.play()
 }
 
-function addPlayerCardClickEvent() {
-  const playerCards = document.querySelector('.cardsPlayer');
-  playerCards.forEach(card => {
-    card.addEventListener('click', () => {
-      handlePlayerCardClick(card.textContent);
-    })
-  })
+function init(){
+  drawCards(5, state.playerSides.player)
+  drawCards(5, state.playerSides.computer)
+  
+  const bgm = document.getElementById("bgm")
+  
+  bgm.play()
 }
 
-
-function addBattleButtonClickEvent() {
-  const battleButton = document.querySelector('#battleButton');
-  battleButton.addEventListener('click', handleBattleButtonClick);
-}
-
-function init () {
-  updateCardsView();
-  addPlayerCardClickEvent();
-  addBattleButtonClickEvent();
-}
-
-init ();
+init()
