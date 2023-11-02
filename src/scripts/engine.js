7b74f0fa8b1e37418a09b34a7c4d47d2c1c50bfd
Frankdias92 <frankmcdias@gmail.com>
2023-11-02 09:21:38 -0300

    :package: build

---
+++ src/scripts/engine.js
@@ -0,0 +1,32 @@
+const state = {
+  score:{
+    playerScore: 0,
+    enemyScore: 0,
+    scoreBox: document.querySelector('#'),
+  },
+  cardSprites: {
+    // avatar: 
+  },
+  fildCards: {
+    player: document.querySelector('#field-card-player'),
+    enemy: document.querySelector('#field-card-enemy'),
+  },
+  actions: {
+
+  },
+}
+
+const cardData = [
+  {
+    id: 0,
+    name: '',
+    type: '',
+    img: '',
+  },
+];
+
+
+
+function init () {}
+
+init ();
